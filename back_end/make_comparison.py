860ad146c0cd158589f2d56ff9209b3d29665e2e
jwons <joe.wonsil@gmail.com>
2017-12-01 14:40:26 -0600

    Edited Comparison to begin to check for multiple matches

--- back_end/make_comparison.py
+++ back_end/make_comparison.py
@@ -1,36 +1,61 @@
 class Comparison():
 
     def __init__(self, articles):
+
+        #Holds all the articles from the web scraper
         self.articles = articles
+
+        #holds all the pairs of articles that are found to be similar
         self.comparisons= []
-        counter = 0
+
+        #Check each article's keywords against all the other keywords to find similar articles
         for article in self.articles:
             for otherArticle in self.articles:
+                #ignore the same article which will be a perfect match
                 if(article.title != otherArticle.title):
+                    #Finds the similar keywords between articles
                     simKeys = set(article.keywords) & set(otherArticle.keywords)
-                    if(len(simKeys) > 5):
+                    #We chose 4 by comapring what actual similar articles returned 
+                    if(len(simKeys) >= 5):
+                        for comparison in self.comparisons:
+                            for art in comparison:
+                                
+                                print(keys)
+                                for key in keys:
+                                    print(art[key]["Title"])
                         Article1 = {article.title:{
                                     "URL":article.url,
                                     "Title":article.title,
                                     "Text":article.text,
                                     "Author":article.authors,
                                     "Leaning":" ",
-                                    "keywords":article.keywords,
+                                    "Keywords":article.keywords,
                                     "Rating":{
                                     "UP":0,
-                                    "DOWN":0}}}
+                                    "DOWN":0},
+                                    "Comparisons":[]}}
                         Article2 = {otherArticle.title:{
                                     "URL":otherArticle.url,
                                     "Title":otherArticle.title,
                                     "Text":otherArticle.text,
                                     "Author":otherArticle.authors,
                                     "Leaning":" ",
-                                    "keywords":otherArticle.keywords,
+                                    "Keywords":otherArticle.keywords,
                                     "Rating":{
                                     "UP":0,
-                                    "DOWN":0}}}
-                        counter = counter + 1
-                        self.comparisons.append({counter:[Article1, Article2]})
-        print(self.comparisons)
+                                    "DOWN":0},
+                                    "Comparisons":[]}}
+
+                        
+                        self.comparisons.append([Article1, Article2])
+                        print(simKeys)
+                        #print(article.title)
+                        #print(otherArticle.title)
+        for comparison in self.comparisons:
+            for temp in comparison:
+                pass
+    
+    def getComparisons(self):
+        return self.comparisons
 
 
