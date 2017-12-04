f1fa39a8faf0bab5695633a647fbe91300f9ccb9
jwons <joe.wonsil@gmail.com>
2017-12-04 14:34:42 -0600

    Pull commit

--- back_end/make_comparison.py
+++ back_end/make_comparison.py
@@ -17,37 +17,7 @@ class Comparison():
                     simKeys = set(article.keywords) & set(otherArticle.keywords)
                     #We chose 4 by comapring what actual similar articles returned 
                     if(len(simKeys) >= 5):
-                        for comparison in self.comparisons:
-                            for art in comparison:
-                                
-                                print(keys)
-                                for key in keys:
-                                    print(art[key]["Title"])
-                        Article1 = {article.title:{
-                                    "URL":article.url,
-                                    "Title":article.title,
-                                    "Text":article.text,
-                                    "Author":article.authors,
-                                    "Leaning":" ",
-                                    "Keywords":article.keywords,
-                                    "Rating":{
-                                    "UP":0,
-                                    "DOWN":0},
-                                    "Comparisons":[]}}
-                        Article2 = {otherArticle.title:{
-                                    "URL":otherArticle.url,
-                                    "Title":otherArticle.title,
-                                    "Text":otherArticle.text,
-                                    "Author":otherArticle.authors,
-                                    "Leaning":" ",
-                                    "Keywords":otherArticle.keywords,
-                                    "Rating":{
-                                    "UP":0,
-                                    "DOWN":0},
-                                    "Comparisons":[]}}
-
-                        
-                        self.comparisons.append([Article1, Article2])
+                        self.comparisons.append([article, otherArticle])
                         print(simKeys)
                         #print(article.title)
                         #print(otherArticle.title)
