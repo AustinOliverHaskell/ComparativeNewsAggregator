bcd1a8614dc32429ee9a117201d8fe6b58aa5e8d
Austin Oliver Haskell <ahaskell@carthage.edu>
2017-12-08 13:36:43 -0600

    Style Bois

--- scrape.js
+++ scrape.js
@@ -2,33 +2,76 @@
 
 const numberOfTiles = 30;
 const columns = 5;
-const rows = 4;
+const rows = 3;
 var rowValue = 1;
+
+var keywords = [];
+
+var colors = ["#8d99ae","#f29559", "#605b56", "#ffffff"];
 
 function scrapeKeywords()
 {
-	console.log("Hello");
+	var database = firebase.database();
+
+	// When the keywords have been populated
+	// create the tiles
+	createTiles();
 }
 
 function createTiles()
 {
+	var id = 0;
 	for (var i = 0; i < rows; i++)
 	{
-		var completeRow = 12;
+		var completeRow = 10;
 
-		for (var q = 0; q < columns; q++)
+		$("#tiles").append(addSpace());
+		while(completeRow > 0)
 		{
-			var value = Math.floor((Math.random() * completeRow) + 1);
+			if (keywords.length <= id)
+			{
+				//break;
+			}
 
-			completeRow -= value;
+			var value = Math.floor((Math.random() * 2) + 1);
 
-			$("#tiles").append(initTile(i, value));
+			completeRow -= value+1;
+
+			var colorScheme = Math.floor(Math.random()*4)
+
+			$("#tiles").append(initTile(id, value, "Untitled"));
+			$("#tiles").append(addSpace());
+
+			$("#"+id+"").css("background-color", colors[colorScheme]);
+
+			// Add the function to select a cookie
+			$("#"+id).click(function() {
+				selectCookie($(this).text());
+			});
+
+			if (colorScheme === 3)
+			{
+				$("#"+id+"").css("color", colors[2]);
+			}
+
+			id++;
 		}
+		$("#tiles").append(addSpace());
 	}
 }
 
-function initTile(pos, size)
+function initTile(pos, size, key)
 {
 
-	return "<div class='tile col-xs-"+size+" id='"+ pos +"'>Hello</div>";
+	return "<div class='tile col-xs-"+size+"' id='"+pos+"'>"+key+pos+"</div>";
+}
+
+function addSpace()
+{
+	return "<div class='tile col-xs-1'> </div>";
+}
+
+function selectCookie(keyword)
+{
+	document.cookie = keyword;
 }
